5be3acd2c7fe38ad28f987190c25009c28595477
erianvc <erianvc.dev@gmail.com>
2022-05-14 19:48:06 -0500

    test: corrects App component tests

--- tests/App.test.jsx
+++ tests/App.test.jsx
@@ -1,16 +1,10 @@
 import { describe, expect, it } from 'vitest'
-import { render, userEvent } from 'test/utils.js'
+import { render } from 'test/utils.js'
 import App from '@/App.jsx'
 
-describe('Simple working test', () => {
-  it('should title be visible', () => {
-    const { getByText } = render(<App />)
-    expect(getByText(/Hello Vite \+ React!/i)).toBeInTheDocument()
-  })
-
-  it('should increment count on click', async () => {
-    const { getByRole, findByText } = render(<App />)
-    userEvent.click(getByRole('button'))
-    expect(await findByText(/count is: 1/i)).toBeInTheDocument()
+describe('App component', () => {
+  it('should be created', () => {
+    const component = render(<App />)
+    expect(component).toBeTruthy()
   })
 })
